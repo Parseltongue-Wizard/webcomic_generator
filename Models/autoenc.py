c6296e262f4a4362d016c5b44df8c6389c4b6041
Parseltongue-Wizard <88839241+Parseltongue-Wizard@users.noreply.github.com>
2024-01-14 19:19:35 +0200

    Update autoenc.py
    Imported needed frameworks and libraries and defined input and output dimensions

--- Models/autoenc.py
+++ Models/autoenc.py
@@ -1,1 +1,10 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Dense, Lambda
+from tensorflow.keras.models import Model
+from tensorflow.keras import backend as K
+import numpy as np
+import matplotlib.pyplot as plt
 
+original_dim = 65 536 # 256*256=65 536 for the rezised comic images
+intermediate_dim = 16 384
+latent_dim = 2
