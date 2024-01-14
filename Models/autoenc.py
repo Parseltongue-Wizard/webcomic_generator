b868b27b79eb8b67cb5da22b2ea5cb6fb4942b07
Parseltongue-Wizard <88839241+Parseltongue-Wizard@users.noreply.github.com>
2024-01-14 20:04:20 +0200

    Update autoenc.py

--- Models/autoenc.py
+++ Models/autoenc.py
@@ -5,6 +5,6 @@ from tensorflow.keras import backend as K
 import numpy as np
 import matplotlib.pyplot as plt
 
-original_dim = 65 536 # 256*256=65 536 for the rezised comic images
+original_dim = 65536 # 256*256=65536 for the rezised comic images
 intermediate_dim = 16 384
 latent_dim = 2
