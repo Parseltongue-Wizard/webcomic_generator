76b81f9fedfad443e8cd82ed69e0b32fac44de51
Parseltongue-Wizard <88839241+Parseltongue-Wizard@users.noreply.github.com>
2024-01-19 18:59:31 +0200

    Update autoenc.py
    Completed the autoencoder, but having some trouble with the input shape not matching the shape of the output

--- Models/autoenc.py
+++ Models/autoenc.py
@@ -8,9 +8,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
-original_dim = 65536 # 256*256=65536 for the rezised comic images
-intermediate_dim = 16 384
-latent_dim = 2
 
 def load_and_normalize_images(folder_path):
     images = []
@@ -28,28 +25,56 @@ def load_and_normalize_images(folder_path):
 
 folder_path = "Resized_data"
 normalized_images = load_and_normalize_images(folder_path)
-X_train, X_test = train_test_split(normalized_images, test_size=0.1, random_state=42)
+X_train, X_temp = train_test_split(normalized_images, test_size=0.3, random_state=42)
+X_val, X_test = train_test_split(X_temp, test_size = 0.5, random_state = 42)
+
 
 class Sampling(tf.keras.layers.Layer):
     def call(self, inputs):
         mean, log_var = inputs
         return tf.random.normal(tf.shape(log_var)) *tf.exp(log_var / 2) + mean
 
-codings_size = 10
+latent_dim = 2
 
-inputs = Input(shape=(original_dim,))
-Z = Dense(intermediate_dim, activation='relu')(inputs)
-Z = Dense(latent_dim, activation='relu')(Z)
+inputs = Input(shape=(256,256))
+Z = Dense(150, activation='relu')(inputs)
+Z = Dense(100, activation='relu')(Z)
 
-codings_mean = tf.keras.layers.Dense(codings_size)(Z) 
-codings_log_var = tf.keras.layers.Dense(codings_size)(Z)
+codings_mean = tf.keras.layers.Dense(latent_dim)(Z) 
+codings_log_var = tf.keras.layers.Dense(latent_dim)(Z)
 codings = Sampling()([codings_mean, codings_log_var])
 variational_encoder = tf.keras.Model(
     inputs=[inputs], outputs=[codings_mean, codings_log_var, codings])
 
-decoder_inputs = tf.keras.layers.Input(shape=[codings_size])
-x = Dense(latent_dim, activation="relu")(decoder_inputs)
-x = Dense(intermediate_dim, activation="relu")(x)
-x = Dense(original_dim)(x)
-outputs = tf.keras.layers.Reshape([original_dim])(x)
+decoder_inputs = tf.keras.layers.Input(shape=[latent_dim])
+x = Dense(100, activation="relu")(decoder_inputs)
+x = Dense(150, activation="relu")(x)
+x = Dense(256*256)(x)
+outputs = tf.keras.layers.Reshape([256,256])(x)
 variational_decoder = tf.keras.Model(inputs=[decoder_inputs], outputs=[outputs])
+
+_, _, codings = variational_encoder(inputs)
+reconstructions = variational_decoder(codings)
+variational_ae = tf.keras.Model(inputs=[inputs], outputs=[reconstructions])
+
+latent_loss = -0.5 * tf.reduce_sum(
+    1 + codings_log_var - tf.exp(codings_log_var) - tf.square(codings_mean),
+    axis=-1
+    )
+variational_ae.add_loss(tf.reduce_mean(latent_loss) / float(original_dim))
+variational_ae.compile(loss='mse', optimizer='nadam')
+history = variational_ae.fit(X_train, X_train, epochs = 25, batch_size = 128, validation_data = (X_val, X_val))
+
+def plot_visualisations(model, images=X_val, n_images=5):
+    visualisations = np.clip(model.predict(images[:n_images]), 0, 1)
+    fig = plt.figure(figsize=(n_images * 1.5, 3))
+    for image_index in range(n_images):
+        plt.subplot(2, n_images, 1 + image_index)
+        plt.imshow(images[image_index], cmap="binary")
+        plt.axis("off")
+        plt.subplot(2, n_images, 1 + n_images + image_index)
+        plt.imshow(visualisations[image_index], cmap="binary")
+        plt.axis("off")
+plot_visualisations(variational_ae)
+plt.show()
+
